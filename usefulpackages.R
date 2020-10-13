fab220ac8b1b04de02d837ae5a4ad265d838f2ee
anonymously-sane <sangk1995@gmail.com>
2020-10-13 22:26:34 +0530

    List of useful packages in R

---
+++ usefulpackages.R
@@ -0,0 +1,51 @@
+#For Data import & Cleaning
+install.packages('DBI')
+install.packages('odbc')
+install.packages('RPostgresSQL')
+install.packages('readxl')
+install.packages('writexl')
+install.packages('haven')
+install.packages('tidyverse')
+install.packages('stringr')
+install.packages('reshape2')
+install.packages('tidyr')
+
+
+
+#For Visualizations
+install.packages('ggvis')
+install.packages('rgl')
+install.packages('htmlwidgets')
+install.packages('leaflet')
+install.packages('dygraphs')
+install.packages('DT')
+install.packages('DiagrammeR')
+install.packages('networkD3')
+install.packages('threejs')
+install.packages(c( "knitr","httpuv"))
+
+#For reporting
+install.packages('shiny')
+install.packages('rmarkdown')
+install.packages('xtable')
+
+#For Time Series
+install.packages('zoo')
+install.packages('quantmod')
+
+
+#For Optimizing
+install.packages('Rcpp')
+install.packages('data.table')
+install.packages('parallel')
+
+#For working with web
+install.packages('XML')
+install.packages('jsonlite')
+install.packages('httr')
+
+#For writing own packages
+install.packages('devtools')
+install.packages('testthat')
+install.packages('roxygen2')
+
